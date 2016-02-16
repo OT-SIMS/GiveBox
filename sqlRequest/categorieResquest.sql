839ac1d4a439903eb941c759ba2c13ddf0883b66
feitor <simon.rispal@insa-lyon.fr>
2016-02-16 10:19:35 +0100

    ajout requete utilisateru

--- sqlRequest/categorieResquest.sql
+++ sqlRequest/categorieResquest.sql
@@ -6,6 +6,8 @@ DELETE FROM [dbo].OffreSet
 DBCC CHECKIDENT ('dbo.OffreSet',RESEED, 0);
 DELETE FROM [dbo].CategorieSet
 DBCC CHECKIDENT ('dbo.CategorieSet',RESEED, 0);
+DELETE FROM [dbo].utilisateurSet
+DBCC CHECKIDENT ('dbo.utilisateurSet',RESEED, 0);
 
 
 bulk INSERT [dbo].CategorieSet
@@ -13,9 +15,13 @@ from 'C:\Users\Simon\Documents\GitHub\GiveBox\sqlRequest\importCategorie.txt'
 with(FIRSTROW = 2, keepnulls, FIELDTERMINATOR = ',' , ROWTERMINATOR = '\n');
 
 bulk INSERT [dbo].fichierTypeSet
-from 'C:\Users\Simon\Documents\GitHub\GiveBox\sqlRequest\importfichierType.txt'
+from 'C:\Users\Simon\Documents\GitHub\GiveBox\sqlRequest\importFichierType.txt'
 with(FIRSTROW = 2, keepnulls, FIELDTERMINATOR = ',' , ROWTERMINATOR = '\n');
 
-select * from [dbo].fichierTypeSet;
+bulk INSERT [dbo].UtilisateurSet
+from 'C:\Users\Simon\Documents\GitHub\GiveBox\sqlRequest\importUtilisateur.txt'
+with(FIRSTROW = 2, keepnulls, FIELDTERMINATOR = ',' , ROWTERMINATOR = '\n');
+
+select * from [dbo].utilisateurSet;
  
  
