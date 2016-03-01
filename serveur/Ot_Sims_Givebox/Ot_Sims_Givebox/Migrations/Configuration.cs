87225d6f05d51e4e3dcbef16fed078b8b1416ee3
feitor <simon.rispal@insa-lyon.fr>
2016-03-01 14:32:55 +0100

    modif configuration

--- serveur/Ot_Sims_Givebox/Ot_Sims_Givebox/Migrations/Configuration.cs
+++ serveur/Ot_Sims_Givebox/Ot_Sims_Givebox/Migrations/Configuration.cs
@@ -5,6 +5,8 @@ namespace Ot_Sims_Givebox.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
     using Ot_Sims_Givebox.Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Ot_Sims_Givebox.Models.ApplicationDbContext>
     {
@@ -15,6 +17,16 @@ namespace Ot_Sims_Givebox.Migrations
 
         protected override void Seed(Ot_Sims_Givebox.Models.ApplicationDbContext context)
         {
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+            var user = new ApplicationUser()
+            {
+             
+                Email = "simon.@rispal.info",
+                EmailConfirmed = true
+            };
+
+            manager.Create(user, "Azerty2302");
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
@@ -27,6 +39,19 @@ namespace Ot_Sims_Givebox.Migrations
 
             //    );
             //
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
+
+            if (roleManager.Roles.Count() == 0)
+            {
+                roleManager.Create(new IdentityRole { Name = "SuperAdmin" });
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+                roleManager.Create(new IdentityRole { Name = "User" });
+            }
+
+
+            var adminUser = manager.FindByEmail("simon.@rispal.info");
+
+            manager.AddToRoles(adminUser.Id, new string[] { "SuperAdmin", "Admin" });
         }
     }
 }
